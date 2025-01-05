50bfeb72f6c1723c409751f0f574fd6e47fb3179
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-05 12:12:14 -0300

    Create form-manga.php

---
+++ form-manga.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiMangás</title>
+</head>
+<body>
+    <form action="enviar-form-manga.php" method="get">
+        digite o nome do mangá <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição do mangá <br>
+        <input type="text" name="descricao"> <br>
+        digite o preço do mangá <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem do mangá <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
